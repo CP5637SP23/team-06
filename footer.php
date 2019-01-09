ee08a32c170e047606711b3ef45bd78cfe45b39c
localboss <localboss@JCUB-MAC19.lan>
2019-01-09 13:04:51 +1000

    new footer

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Git WOrkshop</title>
+  </head>
+  <body>
+    Jiona testing github,ehdfihis hsidhfiuhasf hsaifhishf kahhfih f jsdhhfkj sahkhfahs hh
+  </body>
+  
+</html>
